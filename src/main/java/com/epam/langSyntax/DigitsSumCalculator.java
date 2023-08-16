f72bb4f29556eb5e9fdca0d7edfa655b71a6c848
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-08-16 13:00:57 +0300

    Complited Lang Syntax task

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int result = 0;
+        while (number > 0){
+            result += (number % 10);
+            number /= 10;
+        }
+        System.out.println(result);
     }
-
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,16 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int reversedNumber = 0;
+
+        reversedNumber += (number % 10) *100;
+        number /= 10;
+
+        reversedNumber += (number % 10) *10;
+        number /= 10;
+
+        reversedNumber +=number;
+        System.out.println(reversedNumber);
     }
 
 }
